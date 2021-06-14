ea6c37960941c6b749f2a540b2b6e449e4dbdfc0
渡邉英資 <awatanabe@fminc.xsrv.jp>
2021-06-14 23:46:33 +0900

    first commit

---
+++ src/components/Board.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Board.vue"
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
